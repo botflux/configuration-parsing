52cd04d39f9eefae673db756d087004ec93d7ecb
Victor Mendele <victor.mendele68@gmail.com>
2021-03-14 20:06:06 +0100

    add chain configuration

---
+++ src/ConfigurationParsing/Chain/ChainConfigurationParser.spec.ts
@@ -0,0 +1,46 @@
+import {chainConfigurationParser} from './ChainConfigurationParser'
+import {jsonConfigurationParser} from '../Json/JsonConfigurationParser'
+import {tomlConfigurationParser} from '../Toml/TomlConfigurationParser'
+import {ConfigurationParsingError} from '../ParsableConfiguration'
+
+describe('ChainConfigurationParser', function () {
+    it('should support multiple parsers', function () {
+        // Arrange
+        const jsonConfiguration = `{ "hello": "world" }`
+        const tomlConfiguration = `foo = "bar"`
+        const parser = chainConfigurationParser([
+            jsonConfigurationParser(),
+            tomlConfigurationParser()
+        ])
+
+        // Act
+        const parsedJson = parser.parse(jsonConfiguration)
+        const parsedToml = parser.parse(tomlConfiguration)
+        const supportsJson = parser.supports(jsonConfiguration)
+        const supportsToml = parser.supports(tomlConfiguration)
+
+        // Assert
+        expect(parsedJson).toEqual({ hello: 'world' })
+        expect(parsedToml).toEqual({ foo: 'bar' })
+        expect(supportsJson).toBe(true)
+        expect(supportsToml).toBe(true)
+    })
+
+    it('should throw when not supporting the configuration format', function () {
+        // Arrange
+        const tomlConfiguration = `hello = "foo"`
+        const parser = chainConfigurationParser([
+            jsonConfigurationParser(),
+        ])
+
+        // Act
+        const shouldThrow = () => parser.parse(tomlConfiguration)
+        const supportsToml = parser.supports(tomlConfiguration)
+
+        // Assert
+        expect(shouldThrow).toThrow(
+            new ConfigurationParsingError("Something went wrong while parsing a configuration. There is no parser supporting the given configuration.")
+        )
+        expect(supportsToml).toBe(false)
+    })
+})
---
+++ src/ConfigurationParsing/Chain/ChainConfigurationParser.ts
@@ -0,0 +1,27 @@
+import {ConfigurationParsingError, ParsableConfiguration} from '../ParsableConfiguration'
+
+class ChainConfigurationParser implements ParsableConfiguration {
+    constructor(private readonly parsableConfigurations: ParsableConfiguration[]) {}
+
+    parse(rawConfiguration: string): any {
+        const parsableConfiguration = this.parsableConfigurations
+            .find(parsableConfiguration => parsableConfiguration.supports(rawConfiguration))
+
+        if (!parsableConfiguration) {
+            throw new ConfigurationParsingError(
+                `Something went wrong while parsing a configuration. ` +
+                `There is no parser supporting the given configuration.`
+            )
+        }
+
+        return parsableConfiguration.parse(rawConfiguration)
+    }
+
+    supports(rawConfiguration: string): boolean {
+        return this.parsableConfigurations.reduce((previousValue: boolean, currentValue) =>
+            previousValue || currentValue.supports(rawConfiguration), false)
+    }
+}
+
+export const chainConfigurationParser = (parsableConfigurations: ParsableConfiguration[]): ParsableConfiguration =>
+    new ChainConfigurationParser(parsableConfigurations)
