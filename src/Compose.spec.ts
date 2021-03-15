1eedfa9f8beb444278298a645c1c68a7d431a190
Victor Mendele <victor.mendele68@gmail.com>
2021-03-15 11:36:10 +0100

    add new composition functions

--- src/Compose.spec.ts
+++ src/Compose.spec.ts
@@ -4,7 +4,9 @@ import {
 } from './ConfigurationLoading/Environment/EnvironmentConfigurationLoader'
 import {joiConfigurationValidator} from './ConfigurationValidation/Joi/JoiConfigurationValidator'
 import Joi from 'joi'
-import {fromAlreadyParsableConfiguration} from './Compose'
+import {fromParsedLoadable, fromLoadable} from './Compose'
+import {configurationFileLoader} from './ConfigurationLoading/File/ConfigurationFileLoader'
+import {jsonConfigurationParser} from './ConfigurationParsing/Json/JsonConfigurationParser'
 
 describe('Compose', function () {
     describe('fromAlreadyParsedLoadableConfiguration', function () {
@@ -14,7 +16,7 @@ describe('Compose', function () {
             const validator = joiConfigurationValidator<ProcessEnv>(Joi.object({
                 HELLO: Joi.string()
             }))
-            const composed = fromAlreadyParsableConfiguration(loader)
+            const composed = fromParsedLoadable(loader)
                 .validatingWith(validator)
 
             // Act
@@ -23,6 +25,32 @@ describe('Compose', function () {
             // Assert
             await expect(promise).resolves.toEqual({
                 HELLO: 'world'
+            })
+        })
+    })
+
+    describe('fromLoadable', function () {
+        it('should compose and load configuration components', async function () {
+            // Arrange
+            const loader = configurationFileLoader({ fileLocation: 'testing/config.json' })
+            const parser = jsonConfigurationParser()
+            const validator = joiConfigurationValidator(Joi.object({
+                hello: Joi.object({
+                    db: Joi.string()
+                })
+            }))
+            const composed = fromLoadable(loader)
+                .parsingWith(parser)
+                .validatingWith(validator)
+
+            // Act
+            const promise = composed.create()
+
+            // Assert
+            await expect(promise).resolves.toEqual({
+                hello: expect.objectContaining({
+                    db: 'uri'
+                })
             })
         })
     })
--- src/Compose.ts
+++ src/Compose.ts
@@ -1,5 +1,6 @@
-import {AlreadyParsedLoadableConfiguration} from './ConfigurationLoading/LoadableConfiguration'
+import {AlreadyParsedLoadableConfiguration, LoadableConfiguration} from './ConfigurationLoading/LoadableConfiguration'
 import {ValidatableConfiguration} from './ConfigurationValidation/ValidatableConfiguration'
+import {ParsableConfiguration} from './ConfigurationParsing/ParsableConfiguration'
 
 export interface WithValidatableBuildable<TConfiguration> {
     validatingWith(validatable: ValidatableConfiguration<TConfiguration>): ComposedConfigurationFactory<TConfiguration>
@@ -9,7 +10,7 @@ export interface ComposedConfigurationFactory<TConfiguration> {
     create(): Promise<TConfiguration>
 }
 
-class ConfigurationFactory<TConfiguration> implements ComposedConfigurationFactory<TConfiguration> {
+class ParsedLoadableConfigurationFactory<TConfiguration> implements ComposedConfigurationFactory<TConfiguration> {
     constructor(private readonly validatable: ValidatableConfiguration<TConfiguration>,
                 private readonly parsable: AlreadyParsedLoadableConfiguration<TConfiguration>) {}
 
@@ -19,7 +20,34 @@ class ConfigurationFactory<TConfiguration> implements ComposedConfigurationFacto
     }
 }
 
-export const fromAlreadyParsableConfiguration = <TConfiguration> (parsable: AlreadyParsedLoadableConfiguration<TConfiguration>): WithValidatableBuildable<TConfiguration> => ({
+class LoadableConfigurationFactory<TConfiguration> implements ComposedConfigurationFactory<TConfiguration> {
+    constructor(private readonly loadable: LoadableConfiguration,
+                private readonly parsable: ParsableConfiguration,
+                private readonly validatable: ValidatableConfiguration<TConfiguration>
+    ) {}
+
+    create(): Promise<TConfiguration> {
+        return this.loadable.load()
+            .then(rawConfiguration => this.parsable.parse(rawConfiguration))
+            .then(unvalidatedConfiguration => this.validatable.validate(unvalidatedConfiguration))
+    }
+}
+
+export const fromParsedLoadable = <TConfiguration> (parsable: AlreadyParsedLoadableConfiguration<TConfiguration>): WithValidatableBuildable<TConfiguration> => ({
     validatingWith: (validatable: ValidatableConfiguration<TConfiguration>) =>
-        new ConfigurationFactory<TConfiguration>(validatable, parsable)
+        new ParsedLoadableConfigurationFactory<TConfiguration>(validatable, parsable)
+})
+
+interface WithParsable<TConfiguration> {
+    parsingWith(parsable: ParsableConfiguration): WithValidatableBuildable<TConfiguration>
+}
+
+export const fromLoadable = <TConfiguration> (loadable: LoadableConfiguration): WithParsable<TConfiguration> => ({
+    parsingWith(parsable: ParsableConfiguration): WithValidatableBuildable<TConfiguration> {
+        return {
+            validatingWith(validatable: ValidatableConfiguration<TConfiguration>): ComposedConfigurationFactory<TConfiguration> {
+                return new LoadableConfigurationFactory(loadable, parsable, validatable)
+            }
+        }
+    }
 })
