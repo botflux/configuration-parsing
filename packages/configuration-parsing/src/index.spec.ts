6f3cd6732769823c95e61b7c0c4dab308cfa480c
Victor Mendele <v-mendele@logitud.fr>
2021-05-12 23:13:34 +0200

    test index

---
+++ packages/configuration-parsing/src/index.spec.ts
@@ -0,0 +1,25 @@
+import {
+    parsers,
+    loaders,
+    ValidatableConfiguration,
+    validators,
+    ComposedConfigurationFactory,
+    fromLoadable,
+    LoadableConfiguration,
+    createCacheableConfigurationFactory,
+    TimeInterval,
+    CacheableConfigurationFactoryOptions,
+    ConfigurationValidationError,
+    ConfigurationParsingError,
+    ConfigurationLoadingError,
+    isConfigurationParsingError,
+    isConfigurationLoadingError,
+    fromParsedLoadable,
+    ParsableConfiguration,
+    WithValidatableBuildable,
+    isConfigurationValidationError,
+    ParsedLoadableConfiguration} from './index'
+
+it('pass', function () {
+
+})
